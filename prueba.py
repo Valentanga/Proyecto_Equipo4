023482e8b1e41dd14bceeb22fbe262a0df2c66fc
manuDev99 <lb20280745@toluca.tecnm.mx>
2025-12-03 16:36:37 -0600

    Delete prueba.py

--- prueba.py
+++
@@ -1,7 +0,0 @@
-from pymongo import MongoClient
-
-url = "mongodb+srv://valentinaesquivel880_db_user:JS0RY8c4NFRBFWlk@documentosdbcluster.yqbxntf.mongodb.net/?appName=DocumentosDBCluster"
-client = MongoClient(url)
-db = client['GestorArchivos']
-usuarios = db["Users"]
-print("Conexión exitosa")
